8820a84dcc46dc0c30567865c359db3013efad71
Xinsheng <simson@simsondemacbook-pro.local>
2018-04-11 11:55:55 +0800

    12

--- Xinsheng/AppDelegate.m
+++ Xinsheng/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Override point for customization after application launch.12312
     UIViewController *vc =[[UIViewController alloc]init];
     return YES;
 }
